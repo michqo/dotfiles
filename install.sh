4b2bafdd80cdc3be409c64a081445d990dfb6c12
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2022-12-31 19:55:28 +0100

    Better `install.sh`

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-ln -sf ~/dotfiles/fish/ ~/.config/fish
-ln -sf ~/dotfiles/nvim/ ~/.config/nvim
-ln -sf ~/dotfiles/lvim ~/.config/lvim
-ln -sf ~/dotfiles/gitui/ ~/.config/gitui
+
+# Delete
+rm ~/.config/fish
+rm ~/.config/nvim
+rm ~/.config/lvim
+rm ~/.tmux.conf
+rm ~/.bin
+
+# Create links
+ln -sf ~/dotfiles/.config/fish/ ~/.config/fish
+ln -sf ~/dotfiles/.config/nvim/ ~/.config/nvim
+ln -sf ~/dotfiles/.config/lvim ~/.config/lvim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.bin/ ~/.bin
